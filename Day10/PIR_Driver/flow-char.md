560ff276e69c251ba572be6f7ce1e702bef79b54
sfirebird <sanketgunjal@mirafra.com>
2024-12-06 10:32:49 +0530

    not able to operate on more than one gpio

--- Day10/PIR_Driver/flow-char.md
+++ Day10/PIR_Driver/flow-char.md
@@ -5,3 +5,32 @@ Whatever pin that you want to use for the pir sensor and the led, you need to th
 - request the gpio_pin that you are using
 - set the direction of the requested gpio_pin
 - register the driver
+
+Instead of using the mknode command manually through command line, I can just use the device_create function, these are the steps:
+
+Include the ```<linux/device.h>``` header file
+
+Read more about Linux Device Model [here](https://linux-kernel-labs.github.io/refs/heads/master/labs/device_model.html?highlight=device_create#:~:text=The%20device_create()%20function%20initializes,device%20(%20minor%3Amajor%20).)
+
+1. Get a device number (major number), you can assign your own number or let the kernel give you the number dynamically.
+    c
+    `` 
+    int register_chrdev(unsigned int major, const char *name, const struct file_operation *fops)
+    ``
+2. You need to create a device class in /sys/class/ and this is required for creating a device node.
+    - Create a structure, ```struct class *dev_class```
+    - in ```class.h``` header file, ```struct class * __must_check class_create(const char *name);```
+    - call the api and allocate the return the dev_class, ```dev_class = class_create(CLASS_NAME);```
+3. Create a device belonging to the class, in /dev/ directory.
+
+NOTE:
+- In clean up function, the way you destroy the resources also matters, for example using the function class_destroy() before device_destroy()
+
+
+Pending:
+0. write about device_create();
+1. write about MKDEV()
+2. write about PTR_ERR()
+3. class_destroy()
+4. device_destroy()
+
--- Day10/PIR_Driver/pir_driver.c
+++ Day10/PIR_Driver/pir_driver.c
@@ -4,20 +4,27 @@
 #include <linux/fs.h>
 #include <linux/gpio.h>
 #include <linux/uaccess.h>
+#include <linux/device.h>
 
-#define DRIVER_NAME             "PIR Driver"
+#define DRIVER_NAME             "PIR_Driver"
+
+#define CLASS_NAME              "rpi4"
 
 /*  Define the GPIO Pin interfacing with PIR sensor.
  *  Read from this pin.
+    GPIO17
  */
-#define PIR_SENSOR_IO_PIN       17
+#define PIR_SENSOR_IO_PIN       529
 
 /**
  * Define the GPIO Pin connected to the LED
+   GPIO18
  */
-#define LED_IO_PIN              18
+#define LED_IO_PIN              530
 
 static int driver_major_number;
+static struct class *rpi_class;
+static struct device *rpi_device;
 
 /**
  * @brief
@@ -44,7 +51,7 @@ static int gpio_open(struct inode *inode, struct file *file){
  * @param
  * @return
  */
-static int pir_close(struct inode *inode, struct file *inode){
+static int pir_close(struct inode* inode, struct file* fops){
     pr_info("%s: closed!\n", DRIVER_NAME);
     return 0;
 }
@@ -59,7 +66,20 @@ static int gpio_close(struct inode *inode, struct file *inode){
 }
 */
 
-static int pir_read(struct file *file, char __user *buf, size_t len, loff_t *offset){
+static void gpio_write(int control_status){
+    /*  Depending upon the pir control the led  */
+    if(control_status){
+        gpio_set_value(LED_IO_PIN, control_status);
+        pr_info("%s: Gotcha! Turning on the Alarm!\n", DRIVER_NAME);
+    }
+    else{
+        gpio_set_value(LED_IO_PIN, control_status);
+        pr_info("%s: GPIO Low!\n", DRIVER_NAME);
+    }
+
+}
+
+static ssize_t pir_read(struct file *file, char __user *buf, size_t len, loff_t *offset){
     int get_pir_value;
     
     /*  Read the PIR value  */
@@ -73,29 +93,36 @@ static int pir_read(struct file *file, char __user *buf, size_t len, loff_t *off
     snprintf(buffer, sizeof(buffer), "%d", get_pir_value);
 
     /*  send to the user    */
-    int bytes_sent;
-    if(bytes_sent = copy_to_user(buf, buffer, sizeof(buffer))){
+    ssize_t bytes_sent;
+    if((bytes_sent = copy_to_user(buf, buffer, sizeof(buffer)))){
         pr_err("Failed to send data to user\n");
         return -EFAULT;
     }
 
-    pr_info("%s: successfully read %d!\n", get_pir_value);
+    pr_info("%s: successfully read %d!\n", DRIVER_NAME, get_pir_value);
     return bytes_sent;
 }
 
-static void gpio_write(int control_status){
-    /*  Depending upon the pir control the led  */
-    if(control_status){
-        gpio_set_value(LED_IO_PIN, control_status);
-        pr_info("%s: Gotcha! Turning on the Alarm!\n");
-    }
-    else{
-        
-        gpio_set_value(LED_IO_PIN, control_status);
-        pr_info("%s: GPIO Low!\n");
+static ssize_t gpio_write_from_user(struct file *file, const char __user *buf, size_t len, loff_t *offset){
+    char buffer[2];
+
+    if (copy_from_user(buffer, buf, len)) {
+        pr_err("Failed to receive data from user\n");
+        return -EFAULT;
     }
 
-    return 0;
+    buffer[len] = '\0';
+    int value = simple_strtol(buffer, NULL, 10);
+
+    if (value == 0 || value == 1) {
+        gpio_write(value);  // Set GPIO pin value
+        pr_info("GPIO Device write: %d\n", value);
+    } else {
+        pr_err("Invalid value: GPIO accepts 0 or 1\n");
+        return -EINVAL;
+    }
+
+    return len;
 }
 
 /**
@@ -103,14 +130,15 @@ static void gpio_write(int control_status){
  */
 static struct file_operations gpio_fops = {
     .open = pir_open,
-    .close = pir_close, 
+    .release = pir_close, 
     .read = pir_read, 
+    .write = gpio_write_from_user,
 };
 
 /**
  * Initialization and cleanup functions
  */
-static int __init pir_init(void){
+static int __init pir_driver_init(void){
     int xRetVal;
     
     /*  Request the gpio pin for PIR sensor  */
@@ -143,14 +171,33 @@ static int __init pir_init(void){
         goto resolve;
     }
 
-    /*  Registe the device  */
-    xRetVal = register_chrdev(driver_major_number, DRIVER_NAME, gpio_fops);
-    if(xRetVal){
+    /*  Register the device  */
+    driver_major_number = register_chrdev(0, DRIVER_NAME, (const struct file_operations *)&gpio_fops);
+    if(driver_major_number < 0){
         pr_err("Failed to register the \"%s\" device, Error Returns = %d\n", DRIVER_NAME, xRetVal);
         goto resolve;
     }
+    
+    /*  Create the class    */
+    rpi_class = class_create(CLASS_NAME);
+    if(IS_ERR(rpi_class)){
+        // write an error message for debugging.
+        pr_err("%s: Failed to create the class!\n", DRIVER_NAME);
+        unregister_chrdev(driver_major_number, DRIVER_NAME);
+        return PTR_ERR(rpi_class);
+    }
 
-    pr_info("GPIO Driver loaded\n");
+    /*  Create the device   */
+    rpi_device = device_create(rpi_class, NULL, MKDEV(driver_major_number, 0), NULL, DRIVER_NAME);
+    if(IS_ERR(rpi_device)){
+        // write an error message for debugging.
+        pr_err("%s: Failed to create the device!\n", DRIVER_NAME);
+        class_destroy(rpi_class);
+        unregister_chrdev(driver_major_number, DRIVER_NAME);
+        return PTR_ERR(rpi_device);
+    }
+
+    pr_info("PIR Driver loaded\n");
     return 0;
 
     resolve:
@@ -158,15 +205,21 @@ static int __init pir_init(void){
         return xRetVal;
 }
 
-static int __exit pir_exit(void){
-    unregister_chrdev(driver_major_number, DRIVER_NAME);
+static void __exit pir_driver_exit(void){
     gpio_free(PIR_SENSOR_IO_PIN);
     gpio_free(LED_IO_PIN);
+    device_destroy(rpi_class, MKDEV(driver_major_number, 0));
+    class_destroy(rpi_class);
+    unregister_chrdev(driver_major_number, DRIVER_NAME);
     pr_info("PIR Driver unloaded!\n");
 }
 
+MODULE_AUTHOR("Sanket Gunjal");
+MODULE_DESCRIPTION("Driver to test the PIR Sensor");
+MODULE_LICENSE("GPL");
 
-module_init(pir_init);
-module_exit(pir_exit);
+
+module_init(pir_driver_init);
+module_exit(pir_driver_exit);
 
 
